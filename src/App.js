0d72acf9a9544115b81a4da59c75a8b7a5ad39b5
tarun <mr.tarun0402@gmail.com>
2024-11-19 14:45:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
+import { Route, Routes, Navigate } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
+import Login from './components/Login'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import Trending from './components/Trending'
+import VideoDetailView from './components/VideoDetailView'
+import ThemeAndVideoContext from './context/ThemeAndVideoContext'
+import Gaming from './components/Gaming'
+import SavedVideos from './components/SavedVideos'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+class App extends Component {
+  state = {
+    savedVideos: [],
+    isDarkTheme: false,
+    activeTab: 'Home',
+  }
+
+  changeTab = tab => {
+    this.setState({ activeTab: tab })
+  }
+
+  toggleTheme = () => {
+    this.setState(prevState => ({
+      isDarkTheme: !prevState.isDarkTheme,
+    }))
+  }
+
+  addVideo = video => {
+    const { savedVideos } = this.state
+    const index = savedVideos.findIndex(eachVideo => eachVideo.id === video.id)
+    if (index === -1) {
+      this.setState({ savedVideos: [...savedVideos, video] })
+    } else {
+      savedVideos.splice(index, 1)
+      this.setState({ savedVideos })
+    }
+  }
+
+  removeVideo = id => {
+    const { savedVideos } = this.state
+    const updatedSavedVideos = savedVideos.filter(
+      eachVideo => eachVideo.id !== id,
+    )
+    this.setState({ savedVideos: updatedSavedVideos })
+  }
+
+  render() {
+    const { savedVideos, isDarkTheme, activeTab } = this.state
+    return (
+      <ThemeAndVideoContext.Provider
+        value={{
+          savedVideos,
+          isDarkTheme,
+          activeTab,
+          toggleTheme: this.toggleTheme,
+          addVideo: this.addVideo,
+          changeTab: this.changeTab,
+        }}
+      >
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/trending"
+            element={
+              <ProtectedRoute>
+                <Trending />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/gaming"
+            element={
+              <ProtectedRoute>
+                <Gaming />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/saved-videos"
+            element={
+              <ProtectedRoute>
+                <SavedVideos />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/videos/:id"
+            element={
+              <ProtectedRoute>
+                <VideoDetailView />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/not-found" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/not-found" />} />
+        </Routes>
+      </ThemeAndVideoContext.Provider>
+    )
+  }
 }
 
-export default App;
+export default App
